b285d9d29d753f5d29f0664f4bc005610a330e02
ukoca <ukoca@innova.com.tr>
2023-10-27 17:55:14 +0300

    Swagger-ui added

--- src/main/java/com/example/demo/controller/demo/DemoController.java
+++ src/main/java/com/example/demo/controller/demo/DemoController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller.demo;
 
+import com.example.demo.config.SpringFoxConfig;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
+@Api(tags = {SpringFoxConfig.DEMO_TAG})
 public class DemoController {
 
     @GetMapping
